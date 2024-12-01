f393caa0ae946ad02183c05930a40645d278eb51
ttn1401 <trantuannhut140103@gmail.com>
2024-12-01 10:19:45 +0700

    init project

---
+++ dienthoai/include/footer.php
@@ -0,0 +1,16 @@
+<div ><hr>
+	<img class="logoft" src="include/img/footer.PNG">
+</div>
+<div class="container-fluid">
+	 <div class="row bg">
+ 	<div class="col-md-5 ft">
+ 		<div >
+ 			<div><strong>Akada Shop mobile</strong></div>
+ 			<div>Số 451 Hoàng Diệu - Thành phố Đà Nẵng</div>
+			<div>Opening from Monday - Sunday ( 9am- 10pm)</div>
+			<div>Tổng đài (8:30 - 20:30) : 1900 2012</div>
+ 		</div>
+ 	</div>
+
+</div>
+</div>
